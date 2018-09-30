1ea69e9b2a4fc5c5bdca6d2a2137808b2dbb7c27
Mohammed Saudi <msaudi.cse@gmail.com>
2018-09-30 17:02:55 +0300

    Adding Index page and styling

--- public/index.php
+++ public/index.php
@@ -1,10 +1,35 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-	<head>
-		<meta charset="utf-8">
-		<title></title>
-	</head>
-	<body>
-		<?php echo "Public Area" ?>
-	</body>
+<!doctype html>
+<html lang="en">
+<head>
+	<title>YomyShop - Istanbul</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" media="all" href="css/public.css">
+</head>
+
+<body>
+	<header>
+		<h1>
+			<a href="">
+				<img class="yomy-icon" src= "img/yomy-icon.png" /> </br></br>
+				YomyShop
+			</a>
+		</h1>
+	</header>
+
+	<div id="main">
+		<ul id="menu">
+			<li><a href="#">View Meals</a></li>
+			<li><a href="#">About Us</a></li>
+		</ul>
+
+	</div>
+	<div class="expanding-wrapper">
+		<img class="expanding-wrapper-img" src="img/bg.jpeg" alt="">
+	</div>
+	<footer>
+
+	</footer>
+
+
+</body>
 </html>
